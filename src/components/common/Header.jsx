6aa37ac2be3d223eaffed937d7e541ff09bb90fd
MaxGuraevskiy <maximguraevskiy@gmail.com>
2021-08-07 20:40:31 +0300

    header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react"
+import "./header-styles.css"
 
-const Header = (_) => {
-  return (<></>);
+const Header = _ => {
+  return (
+    <>
+      <div className="header">
+        <div className="header-item name-n-number-item">
+          <p className="name-text">Печать чертежей</p>
+          <p className="phone-text">+7 (926) 205-00-21</p>
+        </div>
+        <div className="header-item date-item">
+          <div className="date-block">
+            <p className="date-text">12 сентября, 2021г.</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default Header;
+export default Header
