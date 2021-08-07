943e90fcd56ba9bb65a057a03123555f16c3e098
mskKote <msk.vitaly@gmail.com>
2021-08-07 22:49:36 +0300

    right section

--- src/components/order/Bill.jsx
+++ src/components/order/Bill.jsx
@@ -4,10 +4,29 @@ import './order.css'
 
 const Bill = () => {
   return (<div id="bill">
-    <h2 id="userName">Максим Гураевский</h2>
-    <input id="balance" /><label htmlFor="balance">Сумма на счету</label>
-    <input id="posibleDebt" /><label htmlFor="posibleDebt">Сумма на счету</label>
-    <input id="personalSale" /><label htmlFor="personalSale">Сумма на счету</label>
+    <h2 id="userName">Максим Гураевский Дмитриевич</h2>
+    <div id="input-bills">
+      <div className="input-bill">
+        <input className="input-bill" id="balance" />
+        <label htmlFor="balance">Сумма на счету</label>
+      </div>
+      <div className="input-bill">
+        <input className="input-bill" id="posibleDebt" />
+        <label htmlFor="posibleDebt">Возможный кредит</label>
+      </div>
+      <div className="input-bill">
+        <input className="input-bill" id="personalSale" />
+        <label htmlFor="personalSale">Личная скидка</label>
+      </div>
+    </div>
+
+    <div id="priceDiv">
+      <p>Итоговая цена:</p>
+      <p id="price">250 000₽</p>
+    </div>
+    <div>
+      <button id="makeOrder">Заказать</button>
+    </div>
   </div>);
 }
 
