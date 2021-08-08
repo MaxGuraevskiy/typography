170e724cc1aec764977e6f4c016cb869c389a00d
MaxGuraevskiy <maximguraevskiy@gmail.com>
2021-08-08 04:26:40 +0300

    ChoiceTime

--- src/components/Settings/ChoiceTime.jsx
+++ src/components/Settings/ChoiceTime.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from "react"
+import "./choice-time-styles.css"
 
 const ChoiceTime = () => {
-  return (<></>);
+  const [selected, setSelected] = useState("3ч")
+
+  const selectedHandler = e => {
+    setSelected(e.target.value)
+    console.log(e.target.checked)
+  }
+
+  return (
+    <>
+      <div className="container">
+        {["3ч", "6ч", "12ч", "24ч"].map((x, i) => (
+          <>
+            <input
+              type="radio"
+              value={x}
+              checked={selected === x}
+              onChange={selectedHandler}
+              id={`tab${i + 1}`}
+              name="tab"
+              className="choiceTime"
+            />
+            <label key={i} htmlFor={`tab${i + 1}`} className="choiceTime">
+              {x}
+            </label>{" "}
+          </>
+        ))}
+
+        <div className="line"></div>
+      </div>
+    </>
+  )
 }
 
-export default ChoiceTime;
+export default ChoiceTime
--- src/components/order/Bill.jsx
+++ src/components/order/Bill.jsx
@@ -3,31 +3,33 @@ import React from "react"
 import "./order.css"
 
 const Bill = () => {
-  return (<div id="bill">
-    <h2 id="userName">Максим Гураевский Дмитриевич</h2>
-    <div id="input-bills">
-      <div className="input-bill">
-        <input className="input-bill" id="balance" />
-        <label htmlFor="balance">Сумма на счету</label>
+  return (
+    <div id="bill">
+      <h2 id="userName">Максим Гураевский Дмитриевич</h2>
+      <div id="input-bills">
+        <div className="input-bill">
+          <input className="input-bill" id="balance" />
+          <label htmlFor="balance">Сумма на счету</label>
+        </div>
+        <div className="input-bill">
+          <input className="input-bill" id="posibleDebt" />
+          <label htmlFor="possibleDebt">Возможный кредит</label>
+        </div>
+        <div className="input-bill">
+          <input className="input-bill" id="personalSale" />
+          <label htmlFor="personalSale">Личная скидка</label>
+        </div>
       </div>
-      <div className="input-bill">
-        <input className="input-bill" id="posibleDebt" />
-        <label htmlFor="posibleDebt">Возможный кредит</label>
-      </div>
-      <div className="input-bill">
-        <input className="input-bill" id="personalSale" />
-        <label htmlFor="personalSale">Личная скидка</label>
-      </div>
-    </div>
 
-    <div id="priceDiv">
-      <p>Итоговая цена:</p>
-      <p id="price">250 000₽</p>
+      <div id="priceDiv">
+        <p>Итоговая цена:</p>
+        <p id="price">250 000₽</p>
+      </div>
+      <div>
+        <button id="makeOrder">Заказать</button>
+      </div>
     </div>
-    <div>
-      <button id="makeOrder">Заказать</button>
-    </div>
-  </div>);
+  )
 }
 
 export default Bill
