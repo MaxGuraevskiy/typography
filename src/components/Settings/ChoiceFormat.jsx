b193eb6b5610b57dae5a2b346fe137de8545209c
mskKote <msk.vitaly@gmail.com>
2021-08-10 01:43:58 +0300

    format refactor

--- src/components/Settings/ChoiceFormat.jsx
+++ src/components/Settings/ChoiceFormat.jsx
@@ -1,41 +1,26 @@
 import React, { useState } from "react"
 import "./choice-format-styles.css"
 
-const formatOptions = ["Супер скоросшиватель", "Архивный переплёт", "Пластиковая пружина", "Металлическая пружина"]
+const formatOptions = [
+  "Супер скоросшиватель",
+  "Архивный переплёт",
+  "Пластиковая пружина",
+  "Металлическая пружина"
+]
 
 const ChoiceFormat = () => {
 
-  const [selected, setSelected] = useState("Супер скоросшиватель")
-
-  const selectedHandler = e => {
-    setSelected(e.target.value)
-    console.log(e.target.checked)
-  }
+  const [selected, setSelected] = useState(formatOptions[0])
 
   return (
-    <>
-      <div className="choice-format-container">
-        {formatOptions.map((x, i) => (
-          <>
-            <input
-              type="radio"
-              value={x}
-              checked={selected === x}
-              onChange={selectedHandler}
-              id={`form-tab${i + 1}`}
-              name="form-tab"
-              className="choice-format-btn"
-            />
-            <label key={i} htmlFor={`form-tab${i + 1}`} className="choice-format-label">
-              {x}
-            </label>
-          </>
-        ))}
-      </div>
-
-
-    </>
-  )
+    <div className="choice-format-container">
+      {formatOptions.map((x, i) => (
+        <button key={i}
+          className={"choice-format-btn " + (selected === x ? "selected" : "")}
+          onClick={() => setSelected(formatOptions[i])}
+          children={x} />
+      ))}
+    </div>)
 }
 
 export default ChoiceFormat
