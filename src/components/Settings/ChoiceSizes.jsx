a4704f27f140045447398eeb88f7546a0dcc2f18
mskKote <msk.vitaly@gmail.com>
2021-08-11 00:21:14 +0300

    sizes logic

--- src/components/Settings/ChoiceSizes.jsx
+++ src/components/Settings/ChoiceSizes.jsx
@@ -1,80 +1,88 @@
 import React, { useState, useRef } from 'react';
 import "./choice-sizes-styles.css"
 import horizontall_line from '../../images/horizontall-line.svg'
-
-// import NaturalInput from './NaturalInput';
+import useEventListener from './useEventListener';
 
 const isValid = (value, min, max) => {
-  // console.log('isValid :>> ', min, value);
-  return value !== '' &&
-    (min === undefined || value >= min) &&
-    (max === undefined || value <= max);
+  return value !== '' && value >= min && value <= max;
 }
 
-const NaturalInput = ({ value, min, max, onChange, id, className, inputRef, setActice }) => {
+const NaturalInput = ({ value, min, max, setValue, className, inputRef, setActive }) => {
   const regexp = new RegExp(`^[0-9]*$`); //? new RegExp(`^-?[0-9]*$`);
 
   function onChange(e) {
-    console.log(e.target.value);
-    if (e.target.value === undefined) return
     const newValue = +e.target.value;
     if (regexp.test(newValue) && isValid(newValue, min, max))
-      onChange(newValue);
+      setValue(newValue);
   }
-
   function onBlur() {
-    if (value < min) onChange(min)
-    if (value > max) onChange(max)
-    setActice(false)
+    if (value < min) setValue(min)
+    if (value > max) setValue(max)
+    setActive(false)
   }
-
   function onFocus() {
-    setActice(true)
+    setActive(true)
   }
 
   return (
     <input
-      onFocus={onFocus}
-      ref={inputRef}
-      type="text"
       className={className}
-      value={value}
-      id={id}
       onChange={onChange}
+      onFocus={onFocus}
       onBlur={onBlur}
+      ref={inputRef}
+      value={value}
+      size={3}
     />)
 }
 
-const sizeOptions = ["A4:", "A3:", "A2:", "A1:", "A0:", "A0+:",]
+const InputSize = ({ sizeName, id }) => {
+  const [active, setActive] = useState(false)
+  const [value, setValue] = useState(0)
+  const ref = useRef(null)
 
-const ChoiceSizes = () => {
+  function logKey(e) {
+    if (!active) return
+
+    if (e.code === "ArrowUp") {
+      isValid(+value + 1, 0, 100) && setValue(x => x + 1)
+    }
+    if (e.code === "ArrowDown") {
+      isValid(+value - 1, 0, 100) && setValue(x => x - 1)
+    }
+    if (e.code === "Escape") {
+      ref.current.blur()
+      setActive(false)
+    }
+  }
+  useEventListener("keydown", logKey)
 
   //* Подсчёт по кнопкам
   function count(change) {
-    return () => isValid(+value + +change, 0, 100) && setValue(+value + +change)
+    return () => isValid(+value + +change, 0, 100) && setValue(x => x + +change)
   }
 
-  const [value, setValue] = useState(0)
-  const [active, setActice] = useState(false)
-  const inputRef = useRef(null)
-  return (<>
-    <div className="choice-sizes-container">
-      {sizeOptions.map((x, i) => (
-        <div className="choice-size-item" key={i}>
-          <div className="choice-size-item-body" onClick={() => inputRef.current.focus()}>
-            <label className="paper-size" children={x} />
-            <div className="choice-size-input-group">
-              <label onClick={count(-1)} htmlFor={`size-tab${i + 1}`} children={"-"} className="paper-size-count-minus" />
-              <NaturalInput value={value} min={0} max={100} onChange={setValue} className="choice-size-input" id={`size-tab${i + 1}`} inputRef={inputRef} setActice={setActice} />
-              <label onClick={count(+1)} htmlFor={`size-tab${i + 1}`} children={"+"} className="paper-size-count-plus" />
-            </div>
-          </div>
-          <img className={active ? "inputing" : ""} src={horizontall_line} />
-        </div>
-      ))}
-
+  return (<div className="choice-size-item">
+    <div className="choice-size-item-body" onClick={() => ref.current.focus()}>
+      <label className="paper-size" children={sizeName} />
+      <div className="choice-size-input-group">
+        <label onClick={count(-1)} htmlFor={`size-tab${id}`} children={"-"} className="paper-size-count-minus" />
+        <NaturalInput min={0} max={100} id={`size-tab${id}`} inputRef={ref} className="choice-size-input" value={value} setValue={setValue} setActive={setActive} />
+        <label onClick={count(+1)} htmlFor={`size-tab${id}`} children={"+"} className="paper-size-count-plus" />
+      </div>
     </div>
-  </>)
+    <img className={active ? "inputing" : ""} src={horizontall_line} />
+  </div>)
 }
+
+const sizeOptions = ["A4:", "A3:", "A2:", "A1:", "A0:", "A0+:"]
+
+const ChoiceSizes = () => (
+  <div className="choice-sizes-container">
+    {sizeOptions.map((x, i) =>
+      <InputSize sizeName={x} id={i} key={i} />
+    )}
+  </div>
+)
 
 export default ChoiceSizes;
