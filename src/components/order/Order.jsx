0fd5317a248e74019831885b9cdfae028d4e7ffe
mskKote <msk.vitaly@gmail.com>
2021-08-11 00:45:03 +0300

    try order

--- src/components/order/Order.jsx
+++ src/components/order/Order.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ChoiceNDS from './ChoiceNDS';
 import ChoiceDelivery from './ChoiceDelivery';
@@ -6,10 +6,40 @@ import Bill from './Bill';
 import Map from './Map';
 import './order.css'
 
+const regOptions = ["Физическое лицо", "Юридическое лицо"]
 
 const Order = () => {
+
+  const [selected, setSelected] = useState(regOptions[0])
+
+  const selectedHandler = e => {
+    setSelected(e.target.value)
+    // console.log(e.target.checked)
+  }
+
   return (<>
-    <div style={{ height: "10%" }}>Выбор юр/физ лицо</div>
+    Выбор юр/физ лицо
+    <div style={{ height: "10%" }}>
+      {regOptions.map((x, i) => (
+        <>
+          <input
+            type="radio"
+            value={x}
+            checked={selected === x}
+            onChange={selectedHandler}
+            id={`reg-tab${i + 1}`}
+            name="reg-tab"
+            className="choice-reg-input"
+          />
+          <label key={i} htmlFor={`time-tab${i + 1}`} className="choiceReg">
+            {x}
+          </label>
+          {i !== regOptions.length - 1 && <div className="reg-palka" />}
+        </>
+      ))}
+      <div className="reg-line" />
+    </div>
+
     <div id="orderDetails">
       <div style={{ width: "50%" }}>
         <ChoiceNDS />
