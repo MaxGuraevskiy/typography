bcc7a9a4b6a73ad70f49d55a1c47b1636bc83d65
MaxGuraevskiy <maximguraevskiy@gmail.com>
2021-08-11 11:43:16 +0300

    map

--- src/components/order/Map.jsx
+++ src/components/order/Map.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { YMaps, Map as YMap, Placemark } from 'react-yandex-maps';
+
+const mapData = {
+  center: [55.751574, 37.573856],
+  zoom: 5,
+};
+
+const coordinates = [
+  [55.769545, 37.606613]
+];
 
 const Map = () => {
   return (<div id="map">
-
+    <YMaps>
+      <YMap defaultState={mapData} className="">
+        {coordinates.map(coordinate => <Placemark geometry={coordinate} />)}
+      </YMap>
+    </YMaps>
   </div>);
 }
 
